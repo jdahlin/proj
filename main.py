40edba7265ab909aaa3aee2a28f19dab6cad2002
Johan Dahlin <johan@gnome.org>
2012-04-17 17:33:34 -0300

    Initial analytic integration into the editor ui

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ from gi.repository import Clutter
 from gi.repository import GtkClutter
 from gi.repository import St
 
+import analyze
+
 orig = Clutter.Actor.add_child
 def add_child(parent, actor, **props):
     orig(parent, actor)
@@ -19,38 +21,39 @@ def add_child(parent, actor, **props):
 Clutter.Actor.add_child = add_child
 
 
-class SourceViewArea(GtkClutter.Actor):
+class TextArea(St.BoxLayout):
     def __init__(self):
+        St.BoxLayout.__init__(self, style_class="source_box")
+
+        self.gtk_actor = GtkClutter.Actor()
+        self.add_child(self.gtk_actor, expand=True)
+
         GtkClutter.Actor.__init__(self)
 
-        self._create_language()
-        self._create_buffer()
+        self.text_buffer = self.create_buffer()
+        self._create_default_tags()
         self._create_ui()
-        self.source_view.set_buffer(self.text_buffer)
+        self.view.set_buffer(self.text_buffer)
 
-    def _create_language(self):
-        manager = GtkSource.LanguageManager.get_default()
-        self.language = manager.get_language('python')
-
-    def _create_buffer(self):
-        self.text_buffer = GtkSource.Buffer()
-        self.text_buffer.set_language(self.language)
+    def _create_default_tags(self):
         tag_table = self.text_buffer.get_tag_table()
         self._default_tag = Gtk.TextTag(name='default')
         tag_table.add(self._default_tag)
         self._default_tag.props.font = Pango.FontDescription('Ubuntu Mono 12')
 
     def _create_ui(self):
-        bin = self.get_widget()
-        self.show()
+        bin = self.gtk_actor.get_widget()
+        self.gtk_actor.show()
 
         sw = Gtk.ScrolledWindow()
         bin.add(sw)
         sw.show()
 
-        self.source_view = GtkSource.View()
-        sw.add(self.source_view)
-        self.source_view.show()
+        self.view = self.create_view()
+        sw.add(self.view)
+        self.view.show()
+
+    # Public
 
     def add_from_file(self, filename):
         # FIXME: async
@@ -60,8 +63,42 @@ class SourceViewArea(GtkClutter.Actor):
         start, end = self.text_buffer.get_bounds()
         self.text_buffer.apply_tag(self._default_tag, start, end)
 
+    def create_buffer(self):
+        text_buffer = Gtk.TextBuffer()
+        return text_buffer
+
+    def create_view(self):
+        view = Gtk.TextView()
+        return view
+
+    def set_content(self, content):
+        self.text_buffer.props.text = content
+
+    def get_content(self):
+        return self.text_buffer.props.text
+
     def grab_focus(self):
-        self.source_view.grab_focus()
+        self.view.grab_focus()
+
+
+class SourceViewArea(TextArea):
+    def __init__(self):
+        super(SourceViewArea, self).__init__()
+        self._language_manager = GtkSource.LanguageManager.get_default()
+
+    def create_buffer(self):
+        text_buffer = GtkSource.Buffer()
+        return text_buffer
+
+    def create_view(self):
+        view = GtkSource.View()
+        return view
+
+    def add_from_file(self, filename):
+        super(SourceViewArea, self).add_from_file(filename)
+        if filename.endswith('.py'):
+            self.text_buffer.set_language(
+                self._language_manager.get_language('python'))
 
 
 class App:
@@ -93,28 +130,30 @@ class App:
                                   height=rect.height - 28)
         self.stage.add_child(background)
 
-        left_box = St.BoxLayout(style_class="left_box")
-        background.add_child(left_box, y_fill=False, y_align=St.Align.START)
+        left_box = St.BoxLayout(style_class="left_box", vertical=True)
+        background.add_child(left_box)
 
-        right_box = St.BoxLayout(style_class="right_box")
-        background.add_child(right_box, expand=True,
-                             x_align=St.Align.END, x_fill=False,
-                             y_align=St.Align.START, y_fill=False)
-        right_box.hide()
+        right_box = St.BoxLayout(style_class="right_box", vertical=True)
+        background.add_child(right_box)
 
-        box = St.BoxLayout(style_class="source_box")
-        left_box.add_child(box, expand=True)
+        source_view = SourceViewArea()
+        source_view.add_from_file('example.py')
+        left_box.add_child(source_view, expand=True)
 
+        v = analyze.analyze(source_view.get_content())
+        for reference in v.references.values():
+            if not reference.value:
+                continue
+            doc = reference.value.__doc__
+            if not doc:
+                continue
+
+            info_ = TextArea()
+            info_.set_content(doc.encode('utf-8'))
+            right_box.add_child(info_, expand=True)
+
+        source_view.grab_focus()
         self.clutter.show()
-
-        self.create_source_view(box)
-
-    def create_source_view(self, main_box):
-        gtk_actor = SourceViewArea()
-        main_box.add_child(gtk_actor, expand=True, x_fill=True,
-                          )
-        gtk_actor.add_from_file('example.py')
-        gtk_actor.grab_focus()
 
     def on_key_press_event(self, window, event):
         if event.keyval == Gdk.KEY_Escape:
